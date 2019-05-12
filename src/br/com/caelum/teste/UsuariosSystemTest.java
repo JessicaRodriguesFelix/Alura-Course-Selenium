2d5da9342f228592700cf9e3526559d35a073d7c
Rodrigues de Souza <jars@gft.com>
2019-05-12 00:30:34 -0300

    first class to test Add User functionality

--- src/br/com/caelum/teste/UsuariosSystemTest.java
+++ src/br/com/caelum/teste/UsuariosSystemTest.java
@@ -2,42 +2,45 @@ package br.com.caelum.teste;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class UsuariosSystemTest {
 	
-	@Test
 	
-	public void deveAdicionarUmUsuario() {
-		
+	private FirefoxDriver driver;
+	
+	
+	@Before	
+	public void inicializa() {	
 		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
-		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://localhost:8080/usuarios/new");
-		
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-		WebElement email = driver.findElement(By.name("usuario.email"));
-		
-		nome.sendKeys("Jessica Rodrigues de Souza");
-		email.sendKeys("jessicarodrigues_felix@hotmail.com");
-		
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		botaoSalvar.click();
-		
-		//garantir que a Jessica foi adicionada
-		
-		boolean achouNome = driver.getPageSource().contains("Jessica Rodrigues de Souza");
-		boolean achouEmail = driver.getPageSource().contains("jessicarodrigues_felix@hotmail.com");
-		
-		assertTrue(achouNome);
-		assertTrue(achouEmail);
-		
-		driver.close();
+		this.driver = new FirefoxDriver();
 		
 	}
+	
+	@Test
+	
+	public void deveAdicionarUmUsuario() throws Exception {
+		
+		UsuariosPage usuarios = new UsuariosPage(driver);
+		
 
+		usuarios.visita();
+		Thread.sleep(3000);
+        usuarios.novo().cadastra("Jéssica Rodrigues de Souza", "jessicarodrigues_felix@hotmail.com");
+
+        Thread.sleep(3000);
+        
+        assertTrue(usuarios.existeNaListagem("Jéssica Rodrigues de Souza", "jessicarodrigues_felix@hotmail.com"));	
+		}
+	
+	@After 
+	public void finaliza() {
+		driver.close();	
+	}	
 }
+
+
+
